f685e520e2d8e72ac4b43acf9f0330551cb70bd1
ganeshbelote <ganeshbelote18@gmail.com>
2025-02-27 19:47:37 +0530

    first commit

---
+++ backend/routes/course.route.js
@@ -0,0 +1,24 @@
+import express from "express";
+import { addCourse, cancleEnroll, checkEnrollment, enrollCourse, getAllEnrollments, getCourseDetail } from "../controllers/course.controller.js";
+import { getAllCourses } from "../controllers/course.controller.js";
+import { ensureAuth } from '../middlewares/ensureAuth.js';
+import { upload } from '../middlewares/multer.js';
+
+const router = express.Router();
+
+router.route("/").post(
+  upload.fields([{ name: "thumbnail", maxCount: 1 }, { name: "videos", maxCount: 5 }]),
+  addCourse
+)
+
+        .get(getAllCourses)
+
+router.route('/courseDetails').post(getCourseDetail)
+router.route('/enrollCourse').post(enrollCourse)
+router.route('/cancleEnroll').post(cancleEnroll)
+router.route('/checkEnrollment').post(checkEnrollment)
+router.route('/getAllEnrollments').post(getAllEnrollments)
+
+
+
+export default router;
